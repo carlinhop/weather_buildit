49d9a4b0eb44bc73558bf480b878c19f48abc17e
carlinhop <carlinhop@gmail.com>
2018-02-18 14:41:36 +0000

    Added a search bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
+import TextField from "material-ui/TextField";
 import "./App.css";
 import Bar from "./Components/Bar";
 import DaysList from "./Components/DaysList";
--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -1,11 +1,24 @@
 import React from "react";
-import AppBar from "material-ui/AppBar";
+import {
+  Toolbar,
+  ToolbarGroup,
+  ToolbarSeparator,
+  ToolbarTitle
+} from "material-ui/Toolbar";
+import TextField from "material-ui/TextField";
+import FlatButton from "material-ui/FlatButton";
 
 const Bar = () => (
-  <AppBar
-    title="Weather BuildIt"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
+  <Toolbar>
+    <ToolbarGroup>
+      <ToolbarTitle text="Weather BuildIt" />
+    </ToolbarGroup>
+
+    <ToolbarGroup>
+      <TextField hintText="City" />
+      <FlatButton label="Search" primary={true} />
+    </ToolbarGroup>
+  </Toolbar>
 );
 
 export default Bar;
