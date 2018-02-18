7f940b74785bc5d498afbb72dcdf34de28f73aeb
carlinhop <carlinhop@gmail.com>
2018-02-18 14:23:24 +0000

    Added Bar and DaysLiss

---
+++ src/Components/DaysList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+
+class DaysList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="DaysList">
+        <div>List</div>
+        <p>item 1</p>
+      </div>
+    );
+  }
+}
+
+export default DaysList;
