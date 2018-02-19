be8677d02e78ce3782af74f876f0a2aaeac9ae66
carlinhop <carlinhop@gmail.com>
2018-02-19 22:46:17 +0000

    Added support for https from heroku

--- src/Actions/ActionCreators.js
+++ src/Actions/ActionCreators.js
@@ -1,7 +1,7 @@
 export const GETFORECAST = "GETFORECAST";
 
 const urlForecast =
-  "http://api.openweathermap.org/data/2.5/forecast?q=city&units=metric&mode=json&APPID=b57750b61d184fe48f10456a380f0dc5";
+  "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=city&units=metric&mode=json&APPID=b57750b61d184fe48f10456a380f0dc5";
 
 export function getForecast(daysOfForecast) {
   return {
